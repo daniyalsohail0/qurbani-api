233fc598e81edceebb94b79486fa661d8593dabf
Daniyal Sohail <daniyalsohail0@gmail.com>
2025-06-03 09:44:02 +0100

    cors url added

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ async function main() {
 
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: ["http://localhost:3000", "https://qurbani-2025.vercel.app"],
       credentials: true,
     })
   );
